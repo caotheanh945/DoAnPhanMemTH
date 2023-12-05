b8972af9cbca25e00274b3c75904b765af45d0ff
NhanNhan <caotheanh945@gmail.com>
2023-12-05 16:24:09 +0700

    BackUp WebBanSach

---
+++ MvcBookStore/MvcBookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,68 @@
+﻿using MvcBookStore.Models;
+using PagedList;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+namespace MvcBookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        // Use DbContext to manage database
+        QLBANSACHEntities database = new QLBANSACHEntities();
+        private List<SACH> LaySachMoi(int soluong)
+        {
+            // Sắp xếp sách theo ngày cập nhật giảm dần, lấy đúng số lượng sách cần
+            // Chuyển qua dạng danh sách kết quả đạt được
+            return database.SACHes.OrderByDescending(sach =>
+            sach.Ngaycapnhat).Take(soluong).ToList();
+        }
+        // GET: BookStore
+        public ActionResult Index(int? page)
+        {
+
+            int pageSize = 5;
+            int pageNum = (page ?? 1);
+            // Giả sử cần lấy 5 quyển sách mới cập nhật
+            var dsSachMoi = LaySachMoi(15);
+            return View(dsSachMoi.ToPagedList(pageNum, pageSize));
+        }
+        public ActionResult LayChuDe()
+        {
+            var dsChuDe = database.CHUDEs.ToList();
+            return PartialView(dsChuDe);
+        }
+        public ActionResult LayNhaXuatBan()
+        {
+            var dsNhaXB = database.NHAXUATBANs.ToList();
+            return PartialView(dsNhaXB);
+        }
+
+        public ActionResult SPTheoChuDe(int id, int? page)
+        {
+
+            int pageSize = 5;
+            int pageNum = (page ?? 1);
+
+
+            var dsSachTheoChuDe = database.SACHes.Where(sach => sach.MaCD == id).ToList();
+            return View("Index", dsSachTheoChuDe.ToPagedList(pageNum, pageSize));
+        }
+
+        public ActionResult SPTheoNXB(int id,int? page)
+        {
+
+            int pageSize = 5;
+            int pageNum = (page ?? 1);
+
+
+            var dsSachNXB = database.SACHes.Where(sach => sach.MaNXB == id).ToList();
+            return View("Index", dsSachNXB.ToPagedList(pageNum,pageSize));
+        }
+
+        public ActionResult Details(int id)
+        {
+            var sach = database.SACHes.FirstOrDefault(s => s.Masach == id);
+            return View(sach);
+        }
+    }
+}
---
+++ MvcBookStore/MvcBookStore/Controllers/NguoiDungController.cs
@@ -0,0 +1,88 @@
+﻿using MvcBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBookStore.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        QLBANSACHEntities database = new QLBANSACHEntities();
+
+
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult DangKy(KHACHHANG kh)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(kh.HoTenKH))
+                    ModelState.AddModelError(string.Empty, "Họ tên không được để trống");
+                if (string.IsNullOrEmpty(kh.TenDN))
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập không được để trống");
+                if (string.IsNullOrEmpty(kh.Matkhau))
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không được để trống");
+                if (string.IsNullOrEmpty(kh.Email))
+                    ModelState.AddModelError(string.Empty, "Email không được để trống");
+                if (string.IsNullOrEmpty(kh.DienthoaiKH))
+                    ModelState.AddModelError(string.Empty, "Số điện thoại không được để trống");
+
+
+                var khachhang = database.KHACHHANGs.FirstOrDefault(k => k.TenDN == kh.TenDN);
+                if (khachhang != null)
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập này đã được sử dụng");
+                if (ModelState.IsValid)
+                {
+                    database.KHACHHANGs.Add(kh);
+                    database.SaveChanges();
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return RedirectToAction("DangNhap");
+        }
+
+
+
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        // GET: NguoiDung
+       
+        [HttpPost]
+        public ActionResult DangNhap(KHACHHANG kh)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(kh.TenDN))
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập không được để trống");
+                if (string.IsNullOrEmpty(kh.Matkhau))
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không được để trống");
+                if (ModelState.IsValid)
+                {
+                    var khach = database.KHACHHANGs.FirstOrDefault(k => k.TenDN == kh.TenDN && k.Matkhau == kh.Matkhau);
+                    if (khach != null)
+                    {
+                        ViewBag.ThongBao = "Chúc mừng đăng nhập thành công";
+                        Session["TaiKhoan"] = khach;
+                    }
+                    else
+                        ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+            return View();
+        }
+    }
+}
